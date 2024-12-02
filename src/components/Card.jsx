e31e26c5708028ef635733141b5e2e2bcfaccf83
VitaFrancesco <vitafrancesco00@gmail.com>
2024-12-02 15:34:52 +0100

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+import style from './Card.module.css';
+
+export default function Card(props) {
+    return (
+        <div className={style.card}>
+            <div className={style.cardImg}>
+                <img src={props.image} alt="#" />
+            </div>
+            <div className={style.cardBody}>
+                <h2>{props.title}</h2>
+                <div className={style.tags}>
+                    {
+                        props.tags.map((tag) => <div className={style[tag]}>{tag}</div>)
+                    }
+                </div>
+                <p>{props.content}</p>
+                <button>Leggi di più</button>
+            </div>
+        </div>
+    );
+};
